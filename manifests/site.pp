d08b7bb105a02f423f0321685f3d16b3253889d4
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-10 15:17:45 -0400

    Remove autorefresh

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,6 @@ class base {
     gpgcheck    => 1,
     gpgkey      => 'https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch',
     enabled     => 1,
-    autorefresh => 1,
     type        => 'rpm-md',
   }
 
